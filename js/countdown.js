aa10fcee24ffdd5dbdf493849763ef3861e96342
Mikel <canteromikel@gmail.com>
2022-04-13 20:10:13 -0500

    New: countdown version added

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,10 @@
 let countDownDate = new Date("2022-08-19 20:00:00").getTime();
 
+let daysDiv = document.getElementById('days');
+let hoursDiv = document.getElementById('hours');
+let minsDiv = document.getElementById('mins');
+let secsDiv = document.getElementById('secs');
+
 window.onload = () => {
     setInterval(function() {
         let now = new Date().getTime();
@@ -9,11 +14,6 @@ window.onload = () => {
         let hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
         let seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
-
-        let daysDiv = document.getElementById('days');
-        let hoursDiv = document.getElementById('hours');
-        let minsDiv = document.getElementById('mins');
-        let secsDiv = document.getElementById('secs');
 
         daysDiv.innerText = days;
         hoursDiv.innerText = hours;
